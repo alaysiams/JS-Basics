8d761036260e378d2f1042eb1fb58df46078c4f3
alaysiams <alaysiams@gmail.com>
2026-02-28 03:19:51 -0800

    JS Skills_v1

--- 1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
@@ -1,10 +1,13 @@
-// document.getElementById("count").innerText = 5
-
-let count = 5
-
-console.log(count)
-
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints += 50;
+console.log(bonusPoints);
+
 // Decrease it down to 25, and then finally increase it to 70
+bonusPoints -= 25;
+console.log(bonusPoints);
 
-
+bonusPoints += 70;
+console.log(bonusPoints);
