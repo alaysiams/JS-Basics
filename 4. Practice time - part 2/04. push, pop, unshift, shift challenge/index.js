1b474be18705bf89aadadbf20e8eab2b6cdad3af
alaysiams <alaysiams@gmail.com>
2026-03-04 04:27:28 -0800

    JS v_3

--- 4. Practice time - part 2/04. push, pop, unshift, shift challenge/index.js
+++ 4. Practice time - part 2/04. push, pop, unshift, shift challenge/index.js
@@ -1,7 +1,15 @@
-let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
 
-// You need to help me fixup the largeCountries array so that 
+// You need to help me fixup the largeCountries array so that
 // China and Pakistan are added back into their respective places
 
 // Use push() & pop() and their counterparts unshift() & shift()
 // Google how to use unshift() and shift()
+
+largeCountries.shift(); // takes away 0 index
+largeCountries.unshift("China"); // adds 0 index
+
+largeCountries.pop(); // takes away last index
+largeCountries.push("Pakistan"); // adds last index
+
+console.log(largeCountries);
